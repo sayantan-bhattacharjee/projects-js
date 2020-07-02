cd9389dca84c812dace2b14aeb919bacdde18871
sapa6662 <s.bhattacharjee00@gmail.com>
2020-07-03 01:01:46 +0530

    plz commit

--- router.js
+++ router.js
@@ -1,16 +1,44 @@
 window.onload = function() {
    
-    // Grabbing all active attribute routes
-    let activeRoutes = Array.from(document.querySelectorAll('[route]'));
-
-    console.log(activeRoutes);
-
     // Create a Router Constructor
 
     let Router = function(name, routes) {
         return {
             name: name,
             routes: routes
+        }
+    };
+    
+     //  grab all active attributes routes
+
+    let activeRoutes = Array.from(document.querySelectorAll('[route]'));
+
+    function navigate(event) {
+        var route = event.target.attributes[0].value;
+        var routeInfo =  myFirstRouter.routes.filter(function(r) {
+         return r.path === route;
+    })[0];
+    if(!routeInfo) {
+        window.history.pushState({}, '', 'error')
+        view.innerHTML = 'No route exists with this path'
+    }
+    else{
+        window.history.pushState({}, '', routeInfo.path);
+        console.log(window.history);
+        view.innerHTML = 'You have clicked the ' + routeInfo.name + ' route'
+    }
+    };
+
+    // add event listeners
+ 
+    activeRoutes.forEach(function(route) {
+        route.addEventListener('click', navigate, false);
+    });
+
+    var Router = function(name,routes) {
+        return {
+            name : name,
+            routes : routes
         }
     };
 
@@ -31,6 +59,7 @@ window.onload = function() {
         },
     ]);
     let currentPath = window.location.pathname;
+    console.log(currentPath);
     if (currentPath === '/') {
        view.innerHTML = "You are on the Home Page."; 
     }
